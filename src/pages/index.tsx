1fdfe49840ab39bc1954983a808c9ed19c78e06d
Rostislav Glizerin <glizerin1108@gmail.com>
2025-04-13 02:46:20 +0300

    Добавил мета-теги и title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,20 @@ import {HeaderStub} from "@/blocks/header/HeaderStub";
 import {MainBlock} from "@/blocks/mainBlock/MainBlock";
 import {FontProvider} from "@/providers/FontProvider";
 import {AboutPolishing} from "@/blocks/aboutPolishingBlock/AboutPolishing";
+import Head from "next/head";
+import config from '../config/config.json'
 
 export default function Home() {
   return (
       <FontProvider>
+          <Head>
+              <title>{config.title}</title>
+              <meta name="description" content="Удаление царапин с дисплея Apple Watch, без вскрытия, гарантия, срочные заказы, оценим по фото."/>
+              <meta name="keywords" content="Полировка apple watch,стекла,полировка,дисплея,экрана,iwatch,watch,эплвотч,эплчасы "/>
+              <meta property="og:url" content="https://karupoiss00.github.io/polishing-landing/"/>
+              <meta property="og:title" content="Удаление царапин со стекла Apple Watch"/>
+              <meta property="og:description" content="Полировка Apple Watch"/>
+          </Head>
         <div>
             <HeaderStub/>
             <Header/>
